f3056d29d6c6a73211b73e695c032a27ab160b8e
Gabriel Rossy <grossy.sjb@gmail.com>
2023-08-30 16:18:20 +0000

    asd

--- main.c
+++ main.c
@@ -3,49 +3,50 @@
 int
 main(int argc, char *argv[])
 {
-    if (argc != 2)
+	if (argc != 2)
 	{
-        fprintf(stderr, "USAGE: monty file\n");
-        return (EXIT_FAILURE);
-    }
+		fprintf(stderr, "USAGE: monty file\n");
+		return (EXIT_FAILURE);
+	}
 
-    FILE *file = fopen(argv[1], "r");
-    if (!file)
+	FILE *file = fopen(argv[1], "r");
+	
+	if (!file)
 	{
-        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-        return (EXIT_FAILURE);
-    }
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		return (EXIT_FAILURE);
+	}
 
-    char line[100];
+	char line[100];
 
-    while (fgets(line, sizeof(line), file) != NULL)
+	while (fgets(line, sizeof(line), file) != NULL)
 	{
-        char command[10];
-        int argument = 0;
-        sscanf(line, "%s %d", command, &argument);
+		char command[10];
+		int argument = 0;
+		sscanf(line, "%s %d", command, &argument);
 
-        if (strcmp(command, "push") == 0)
+		if (strcmp(command, "push") == 0)
 		{
-            push(argument);
-        }
+			push(argument);
+		}
 		else if (strcmp(command, "pall") == 0)
 		{
-            pall(top);
-        }
-        else
-        {
-            fprintf(stderr, "Error: command not found\n");
-        }
-    }
+			pall(top);
+		}
+		else
+		{
+			fprintf(stderr, "Error: command not found\n");
+		}
+	}
 
-    fclose(file);
+	fclose(file);
 
-    while (top != NULL)
+	while (top != NULL)
 	{
-        stack_t *temp = top;
-        top = top->next;
-        free(temp);
-    }
+		stack_t *temp = top;
+		top = top->next;
+		free(temp);
+	}
 
-    return (0);
+	return (0);
 }

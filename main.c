037e758f3c96c265a15cdba1be38635373d4344d
Gabriel Rossy <grossy.sjb@gmail.com>
2023-08-30 16:13:06 +0000

    asd

--- main.c
+++ main.c
@@ -5,7 +5,7 @@ main(int argc, char *argv[])
 {
     if (argc != 2)
 	{
-        fprintf(stderr, "USAGE: %s monty file\n", argv[0]);
+        fprintf(stderr, "USAGE: monty file\n");
         return (EXIT_FAILURE);
     }
 
